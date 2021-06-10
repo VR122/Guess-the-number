4c15777407acf520d57de0611a6367192a26e5da
VR122 <83388288+VR122@users.noreply.github.com>
2021-06-10 20:22:12 +0530

    Update Guess a number.py

--- Guess a number.py
+++ Guess a number.py
@@ -1,11 +1,10 @@
 import random
 secret_number=random.randint(1,10)
-while True:
+guess=0
+while guess!=secret_number:
   guess = int(input("Guess a number between 1 and 10: "))
-  if guess == secret_number:
-    print("Congrats!!.You won!")
-    break
-  elif guess < secret_number:
+  if guess < secret_number:
     print("Guess again. Too low.")
-   elif guess > secret_number : 
-    print("Guess again. Too high")  
+  elif guess > secret_number : 
+    print("Guess again. Too high") 
+print("Congrats!! You win.")
