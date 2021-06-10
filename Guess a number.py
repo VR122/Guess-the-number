16ce9679f7ef2ae216242fb0171c31417cba38d5
VR122 <83388288+VR122@users.noreply.github.com>
2021-06-10 10:12:48 +0530

    Update Guess a number.py

--- Guess a number.py
+++ Guess a number.py
@@ -1,7 +1,7 @@
 import random
 secret_number=random.randint(1,10)
 while True:
-  guess = int(input("Guess a number: "))
+  guess = int(input("Guess a number between 1 and 10: "))
   if guess == secret_number:
     print("Congrats!!.You won!")
     break
